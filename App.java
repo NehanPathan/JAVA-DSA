260bf8e480d094c4f3b44ad6700adb8d9242fbd7
NehanPathan <pathannehan26@gmail.com>
2024-03-17 06:59:04 +0530

    Add BucketSort

--- App.java
+++ App.java
@@ -4,9 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         int[] numbers = { 4, 3, 5, 2, 1 };
 
-        var countingSort = new CountingSort();
+        var bucketSort = new BucketSort();
 
-        countingSort.sort(numbers, 5);
+        bucketSort.sort(numbers, 3);
 
         System.out.println(Arrays.toString(numbers));
     }
