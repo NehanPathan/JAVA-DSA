ea791145ef366659e43161df2ae98c660fc5a07d
NehanPathan <pathannehan26@gmail.com>
2024-03-24 06:32:25 +0530

    Add reverseWord  c:; cd 'c:\Users\patha\OneDrive\Desktop\Java DSA'; & 'C:\Program Files\Eclipse Adoptium\jdk-21.0.2.13-hotspot\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\patha\AppData\Roaming\Code\User\workspaceStorage\e19c6222445a8249f14efd09d749fb2c\redhat.java\jdt_ws\Java DSA_b25bffe0\bin' 'App'

--- StringUtils.java
+++ StringUtils.java
@@ -1,5 +1,9 @@
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Stack;
+import java.util.StringJoiner;
 
 public class StringUtils {
     public static int countVowels(String str) {
@@ -31,6 +35,23 @@ public class StringUtils {
 
         return reversed.toString();
 
+    }
+
+    public static String reverseWord(String sentence) {
+        if (sentence == null)
+            return "";
+        String[] words = sentence.split(" ");
+        // StringJoiner reversed = new StringJoiner(" "); // use StringJoiner to join
+        // String Array with Space/any delimiter
+
+        // for (var i = words.length - 1; i >= 0; i--)
+        // reversed.add(words[i]);
+
+        // return reversed.toString();
+
+        // another way, we can use Collection
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
     }
 
 }
